3ee0ccd8b611edb5b71f94d83bbe7834f12c01d3
Shiv Srivastava <1983.shiv@gmail.com>
2022-05-03 14:41:52 +0530

    2nd commit

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 // Master Route
 Route::resource('products', ProductController::class);
 
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+
 // Route::get('/products', [ProductController::class, 'index']);
 
 // Route::post('/products', [ProductController::class, 'store']);
